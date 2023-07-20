f9887580ec1b4bee163a2f65d927dc3f33cec86c
patrikbyz <134887310+patrikbyz@users.noreply.github.com>
2023-07-20 12:10:53 +0200

    Add files via upload

---
+++ form.php
@@ -0,0 +1,26 @@
+<?php
+    $jmeno = $_POST['jmeno'];
+    $nastevnik_email = $_POST['email'];
+    $predmet = $_POST['predmet'];
+    $zprava = $_POST['zprava'];
+
+    $email_from = 'nabelek.patas@gmail.com';
+
+    $email_predmet = 'New form submission';
+
+    $email_body = "User name: $jmeno. \n".
+                    "User email: $nastevnik_email. \n".
+                    "subject: $predmet. \n".
+                    "User message: $zprava. \n";
+
+    $to = 'nabelek.patas@gmail.com';
+    
+    $headrs = "From: $email_from\r\n";
+
+    $headrs .= "Reply-to: $nastevnik_email\r\n";
+    
+    mail($to,$email_predmet,$email_body,$headrs);
+
+    header("Location: kontakt.html");
+    
+    ?>
